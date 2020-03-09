fceb0ff0f88318a8599b70456598aa1bd98f44f6
Heesu Lim <hlim@protonmail.com>
2020-03-09 13:22:08 -0700

    master

---
+++ deploy.ps1
@@ -0,0 +1,43 @@
+$templatePath = './SQLServerTemplate.json'
+$parameterPath = './SQLServer.parameters.json'
+
+#Define functions here
+function createRG {
+    Write-Host "Creating a new resource group."
+
+    New-AzResourceGroup -Name primaryPRT-RG -Location "West US"
+
+    Write-Host "Successfully created new resource group."
+}
+
+function deployTemplate {
+    Write-Host "Deploying template at $templatePath"
+    New-AzResourceGroupDeployment `
+    -Name sqlserverdeployment1 `
+    -ResourceGroupName primaryPRT-RG `
+    -TemplateFile $templatePath `
+    -TemplateParameterFile $parameterPath
+}
+
+#Beginning of "main."
+if(!(Get-Module "Az")) {
+    Write-Host "Importing module Az"
+
+    Import-Module Az
+}
+else {
+    Write-Host "Module Az already loaded, proceeding..."
+}
+
+if(Get-AzContext) {
+    Write-Host "Account detected, moving on..."
+}
+else {
+    Write-Host "Connecting to account..."
+    Connect-AzAccount
+    Write-Host "Account connected."
+}
+
+deployTemplate
+
+#Clear-AzContext
